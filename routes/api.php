43f9eaa920b195ace382135f66cf4670701e7aee
Danny <215646806+d-simeonov@users.noreply.github.com>
2025-09-21 18:36:50 +0300

    Avoid route collision

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\StreamApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+Route::name('api.')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
 
-Route::middleware('auth:api')->apiResource('streams', StreamApiController::class);
+    Route::middleware('auth:api')->apiResource('streams', StreamApiController::class);
+});
